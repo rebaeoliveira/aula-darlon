b91cf83c5a6b8f4351be820ac46e537303002847
rebaeoliveira <115327624+rebaeoliveira@users.noreply.github.com>
2023-08-16 09:36:07 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,3 +2,8 @@ import streamlit as st
 
 st.title("Hello TADS GIT")
 st.write("versao 0.2")
+
+with st.form(key='my form'):
+    submitted = st.form_submit_button('Click me')
+    if submitted:
+        st.balloons()
